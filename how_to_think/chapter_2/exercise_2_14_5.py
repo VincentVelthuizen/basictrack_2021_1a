e2d510aee006c8276bf8f29e2da721a1d7e347b4
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-09-24 14:22:29 +0200

    exercise 5 part 2

--- how_to_think/chapter_2/exercise_2_14_5.py
+++ how_to_think/chapter_2/exercise_2_14_5.py
@@ -1,7 +1,7 @@
-principal_amount = 10000
-frequency = 1
-interest_rate = .01
-duration = 10
+principal_amount = float(input("What is the principal amount?"))
+frequency = int(input("How many times per year is the interest compounded?"))
+interest_rate = float(input("What is the interest rate (per year, as a decimal)"))
+duration = int(input("For what number of years would you like to calculate the compound interest?"))
 
 final_amount = principal_amount * (1 + (interest_rate/frequency)) ** (frequency * duration)
 
