fad4be879dada9ccde2d43f71358e6eefde786e2
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-09-24 17:11:57 +0200

    added a different print to week 2 exercise 1

--- how_to_think/chapter_2/exercise_2_14_1.py
+++ how_to_think/chapter_2/exercise_2_14_1.py
@@ -1,6 +1,7 @@
 sentence_1 = "All work"
-sentence_2 = "and no play"
+sentence_2 = " and no play"
 sentence_3 = "makes Jack"
 sentence_4 = "a dull boy."
 
+print(sentence_1 + sentence_2)
 print(sentence_1, sentence_2, sentence_3, sentence_4)
