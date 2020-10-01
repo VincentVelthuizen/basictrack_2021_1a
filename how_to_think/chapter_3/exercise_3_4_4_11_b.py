9126031615509246880186db56e1dff54f6a4429
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-10-01 13:58:58 +0200

    Chapter 4 exercise 11 (2)

---
+++ how_to_think/chapter_3/exercise_3_4_4_11_b.py
@@ -0,0 +1,23 @@
+import turtle
+
+screen = turtle.Screen()
+cogsworth = turtle.Turtle()
+
+screen.bgcolor("limegreen")
+
+cogsworth.shape("turtle")
+cogsworth.penup()
+cogsworth.color("blue")
+cogsworth.pensize(3)
+
+for _ in range(12):
+    cogsworth.forward(150)
+    cogsworth.pendown()
+    cogsworth.forward(10)
+    cogsworth.penup()
+    cogsworth.forward(20)
+    cogsworth.stamp()
+    cogsworth.goto(0, 0)
+    cogsworth.right(360 / 12)
+
+screen.exitonclick()
