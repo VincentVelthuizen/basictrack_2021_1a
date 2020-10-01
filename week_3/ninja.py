2439820fe210fc460535bd91295c3fe0a447f365
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-10-01 12:56:33 +0200

    ninja code

---
+++ week_3/ninja.py
@@ -0,0 +1,19 @@
+import turtle
+
+paper = turtle.Screen()
+leonardo = turtle.Turtle()
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+for element in range(12):
+    leonardo.color(colors[element % len(colors)])
+    if element % 2 == 0:
+        leonardo.forward(100)
+    else:
+        leonardo.forward(50)
+    leonardo.left(30)
+
+
+
+
+paper.exitonclick()
