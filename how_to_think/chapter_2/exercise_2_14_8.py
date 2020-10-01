949155064759e6b0b5a7a32239ccc1f1d0b976c1
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-10-01 12:55:35 +0200

    Exercise 2 14 8

--- how_to_think/chapter_2/exercise_2_14_8.py
+++ how_to_think/chapter_2/exercise_2_14_8.py
@@ -1,5 +1,6 @@
 current_time = int(input("What is the current hour?"))
 alarm_remaining = int(input("How long do you have to wait?"))
 alarm_goes_off = (current_time + alarm_remaining) % 24
+
 # using a 24 hour makes more sense to me, but I'm Dutch so maybe that is on me?
 print("The alarm will go off at", alarm_goes_off)
