39ab50b60e0363a6dd2a79e1a7f9b191fa9a450d
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-10-01 13:23:35 +0200

    Chapter 4 exercise 2 (2)

---
+++ how_to_think/chapter_3/exercise_2_4_4_2_b.py
@@ -0,0 +1,5 @@
+month_names = ["January", "February", "March", "April", "May", "June",
+               "July", "August", "September", "October", "November", "December"]
+
+for month in month_names:
+    print("One of the months of the year is", month)
