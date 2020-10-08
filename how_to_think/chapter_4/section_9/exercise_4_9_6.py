f2b41bc8645fbd98969c3f5fca1d63ea30ca5412
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-10-08 14:44:20 +0200

    chapter 4.9 exercise 5 side by side

--- how_to_think/chapter_4/section_9/exercise_4_9_6.py
+++ how_to_think/chapter_4/section_9/exercise_4_9_6.py
@@ -1,21 +1,19 @@
+import time
 import turtle
 
 
 screen = turtle.Screen()
 poly = turtle.Turtle()
 
-poly.penup()
-poly.setposition(-150, 0)
-poly.pendown()
-
 number_of_revolutions = 20
 for i in range(number_of_revolutions * 4):
     poly.forward(i * 3)
     poly.right(90)
 
-poly.penup()
-poly.setposition(150, 0)
-poly.pendown()
+# wait for 2 seconds
+time.sleep(2)
+poly.setposition(0, 0)
+screen.clear()
 
 for i in range(number_of_revolutions * 4):
     poly.forward(i * 3)
