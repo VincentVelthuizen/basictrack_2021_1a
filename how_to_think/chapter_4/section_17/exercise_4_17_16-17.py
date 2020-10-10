a75bb1e1087e57c42741ac132e2b0910bce4f528
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-10-10 14:12:36 +0200

    chapter 4.17 exercise 17

--- how_to_think/chapter_4/section_17/exercise_4_17_16-17.py
+++ how_to_think/chapter_4/section_17/exercise_4_17_16-17.py
@@ -2,6 +2,10 @@ def is_factor(f, n):
     return n % f == 0
 
 
+def is_multiple(n, f):
+    return is_factor(f, n)
+
+
 print(is_factor(3, 12))
 print(not is_factor(5, 12))
 print(is_factor(7, 14))
@@ -9,3 +13,11 @@ print(not is_factor(7, 15))
 print(is_factor(1, 15))
 print(is_factor(15, 15))
 print(not is_factor(25, 15))
+
+print()
+
+print(is_multiple(12, 3))
+print(is_multiple(12, 4))
+print(not is_multiple(12, 5))
+print(is_multiple(12, 6))
+print(not is_multiple(12, 7))
