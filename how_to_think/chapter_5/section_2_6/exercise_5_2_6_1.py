6880fcc624c906559ea0ce4f273ddf3bfb67afb7
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-10-15 13:26:54 +0200

    Section 5.2.6 exercise 1

---
+++ how_to_think/chapter_5/section_2_6/exercise_5_2_6_1.py
@@ -0,0 +1,11 @@
+def print_point(point):
+    x, y = point
+    print("({}, {})".format(x, y))
+
+
+def print_coordinates(x, y):
+    print("({}, {})".format(x, y))
+
+
+print_point((1, 3))
+print_coordinates(2, 4)
