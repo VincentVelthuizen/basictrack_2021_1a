66589eae1b80e4be3cc6ba09ac767825cc860dfc
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-10-15 14:02:17 +0200

    Section 5.3.23 exercise 6-7

--- how_to_think/chapter_5/section_3_23/vectors.py
+++ how_to_think/chapter_5/section_3_23/vectors.py
@@ -7,7 +7,38 @@ def add_vectors(vector1, vector2):
     return vector3
 
 
+def scalar_mult(scalar, vector):
+    mult_vector = []
+    for element in vector:
+        mult_vector.append(scalar * element)
+    return mult_vector
+
+
+def dot_product(vec1, vec2):
+    if len(vec1) != len(vec2):
+        return None
+    products = []
+    for v1, v2 in zip(vec1, vec2):
+        products.append(v1 * v2)
+
+    sum_product = 0
+    for product in products:
+        sum_product += product
+
+    return sum_product
+
+
 if __name__ == "__main__":
     print(add_vectors([1, 1], [1, 1]))
     print(add_vectors([1, 2], [1, 4]))
     print(add_vectors([1, 2, 1], [1, 4, 3]))
+    print()
+
+    print(scalar_mult(5, [1, 2]))
+    print(scalar_mult(3, [1, 0, -1]))
+    print(scalar_mult(7, [3, 0, 5, 11, 2]))
+    print()
+
+    print(dot_product([1, 1], [1, 1]))
+    print(dot_product([1, 2], [1, 4]))
+    print(dot_product([1, 2, 1], [1, 4, 3]))
