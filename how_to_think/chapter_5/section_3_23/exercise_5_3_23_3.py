4d1db194f6bf956d1d0535131974b7dc8e399f49
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-10-15 13:37:27 +0200

    Section 5.3.23 exercise 3-4

---
+++ how_to_think/chapter_5/section_3_23/exercise_5_3_23_3.py
@@ -0,0 +1,8 @@
+a = [1, 2, 3]
+print("a: ", a)
+
+b = a[:]
+print("a: ", a, "\nb: ", b)
+
+b[0] = 5
+print("a: ", a, "\nb: ", b)
---
+++ how_to_think/chapter_5/section_3_23/exercise_5_3_23_4.py
@@ -0,0 +1,5 @@
+this = ["I", "am", "not", "a", "crook"]
+that = ["I", "am", "not", "a", "crook"]
+print("Test 1: {0}".format(this is that))
+that = this
+print("Test 2: {0}".format(this is that))
