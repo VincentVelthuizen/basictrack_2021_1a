0f5acc3575c557b3d0a2e1939f6d1cf82502f4b8
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-11-22 21:28:11 +0100

    exercise 11.2.6.1

---
+++ how_to_think/chapter_11/section_2_6/Rectangle.py
@@ -0,0 +1,24 @@
+class Rectangle:
+    """ A class to manufacture rectangle objects """
+
+    def __init__(self, posn, w, h):
+        """ Initialize rectangle at posn, with width w, height h """
+        self.corner = posn
+        self.width = w
+        self.height = h
+
+    def __str__(self):
+        return "({0},{1},{2})".format(self.corner, self.width, self.height)
+
+    def grow(self, delta_width, delta_height):
+        """ Grow (or shrink) this object by the deltas """
+        self.width += delta_width
+        self.height += delta_height
+
+    def move(self, dx, dy):
+        """ Move this object by the deltas """
+        self.corner.x += dx
+        self.corner.y += dy
+
+    def area(self):
+        return self.width * self.height
